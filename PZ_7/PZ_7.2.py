e8293a296b70ae6733800ede0afe605d6387947f
Prog-Maksim <studentsbeloglazov2022@gmail.com>
2024-01-16 10:38:53 +0300

    Обновление практической работы №7

--- PZ_7/PZ_7.2.py
+++ PZ_7/PZ_7.2.py
@@ -1,32 +1,18 @@
 # Дана строка, содержащая цифры и строчные латинские буквы. Если буквы в строке упорядочены по алфавиту, то вывести 0;
-# в противном случае вывести номер первого символа строки, нарушающего алфавитный порядок.
-from colorama import Fore
-from colorama import Style
-
-
-def sort_text(text: str) -> str:
-    """
-    Сортирует строку по алфавиту
-    :param text: строка для сортировки
-    :return: отсортированная строка
-    """
-
-    return "".join((sorted(list(text))))
+# в противном случае вывести номер первого символа строки, нарушающего алфавит.
+from colorama import Fore, Style
 
 
 def check_alphabet(text: str) -> int:
     """
     Функция для проверки строки на соблюдение алфавита
-    Возвращает число -1 - если строка упорядочена, или любое другое число показывающее где допущена ошибка
-    :param text: строка которую нужно проверить
-    :return: результат проверки
+    Возвращает -1, если строка упорядочена, или индекс первого элемента, нарушающего порядок
+    :param text: строка для проверки
+    :return: результат проверки.
     """
-    txt = sort_text(text=text)
-
-    if text != txt:
-        for i in range(len(text)):
-            if text[i] != txt[i]:
-                return i
+    for i in range(len(text) - 1):
+        if text[i] > text[i+1]:
+            return i + 1
     return -1
 
 
@@ -38,6 +24,6 @@ if __name__ == "__main__":
         if result == -1:
             print(f"{Fore.GREEN}Строка упорядочена по алфавиту{Style.RESET_ALL}")
         else:
-            print(f"{Fore.RED}Символ под номером: {result} не соответствует алфавиту{Style.RESET_ALL}")
+            print(f"{Fore.RED}Символ под номером {result} не соответствует алфавиту{Style.RESET_ALL}")
     except KeyboardInterrupt:
         print(f"\n{Fore.YELLOW}Вы завершили работу программы{Style.RESET_ALL}")
