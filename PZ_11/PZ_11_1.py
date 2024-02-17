2887463b0eaedae212d13d7fca7d1d213ef8e958
Prog-Maksim <studentsbeloglazov2022@gmail.com>
2024-02-17 13:25:36 +0300

    Обновление практической №11

--- PZ_11/PZ_11_1.py
+++ PZ_11/PZ_11_1.py
@@ -13,32 +13,32 @@ import random
 import os
 
 
-def create_file(numbers: list) -> None:
-    with open("File_numbers.txt", "w", encoding='utf-8') as file:
+def create_file(numbers: list[int]) -> None:
+    with open("File_numbers.txt", mode="w", encoding='utf-8') as file:
         file.write(str(numbers))
 
 
-def random_number() -> list:
+def random_number() -> None:
     num = random.randint(50, 100)
     rnd = [random.randint(-100, 100) for _ in range(num)]
     create_file(numbers=rnd)
 
 
-def read_file() -> list:
+def read_file() -> list[int]:
     if os.path.isfile("File_numbers.txt"):
-        with open("File_numbers.txt", "r", encoding='utf-8') as file:
+        with open("File_numbers.txt", mode="r", encoding='utf-8') as file:
             numbers = eval(file.read())
 
         return numbers
-    return None
+    return [0]
 
 
 def save_result(data: str) -> None:
-    with open("File_result.txt", "w", encoding='utf-8') as file:
+    with open("File_result.txt", mode="w", encoding='utf-8') as file:
         file.write(data)
 
 
-def processing_function(numbers: list) -> str:
+def processing_function(numbers: list[int]) -> str:
     square_list = [i ** 2 for i in numbers if i > 0]
     str_data = (f"# Исходные данные: \n{numbers}\n\n"
                 f"# Количество элементов: \n{len(numbers)}\n\n"
