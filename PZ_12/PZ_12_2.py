f254d90e315fc1bfc62ee7f8cd93b97c1440c4c7
Prog-Maksim <studentsbeloglazov2022@gmail.com>
2024-02-17 15:12:06 +0300

    Обновление практической №12

--- PZ_12/PZ_12_2.py
+++ PZ_12/PZ_12_2.py
@@ -1,12 +1,10 @@
 # Составить генератор (yield), который выводит из строки только цифры.
 
+
 def main():
     txt = "sgtvrk456ct45634vs56fghdtf89769df464587"
-    for num in txt:
-        if not num.isalpha():
-            yield num
+    yield from [num for num in txt if num.isdigit()]
 
 
 if __name__ == "__main__":
-    for i in main():
-        print(i)
+    [print(i) for i in main()]
