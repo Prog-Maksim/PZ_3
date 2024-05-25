317458be557da1eba6dd3bb3b60e637ce006f6a8
Maksim Beloglazov <maksimbel20170@gmail.com>
2024-05-25 15:41:48 +0300

    обновление 16 практической

--- PZ_16/PZ_16_1.py
+++ PZ_16/PZ_16_1.py
@@ -1,4 +1,7 @@
-class counter:
+import pickle
+
+
+class Counter:
     def __init__(self):
         self.counter_num = 0
 
@@ -8,10 +11,31 @@ class counter:
     def decrement(self):
         self.counter_num -= 1
 
+def save_def(filename, instance):
+    with open(filename, 'wb') as f:
+        pickle.dump(instance, f)
 
-count = counter()
-print(count.counter_num)  # Вывод: 0
-count.increment()
-print(count.counter_num)  # Вывод: 1
-count.decrement()
-print(count.counter_num)  # Вывод: 0
+def load_def(filename):
+    with open(filename, 'rb') as f:
+        return pickle.load(f)
+
+
+# Сохранение экземпляров класса
+counter1 = Counter()
+counter1.increment()
+
+counter2 = Counter()
+counter2.increment()
+counter2.increment()
+
+counter3 = Counter()
+counter3.decrement()
+
+save_def('counters.data', [counter1, counter2, counter3])
+
+# Загрузка экземпляров класса из файла
+loaded_counters = load_def('counters.data')
+
+# Демонстрация загруженных экземпляров
+for counter in loaded_counters:
+    print(counter.counter_num)
